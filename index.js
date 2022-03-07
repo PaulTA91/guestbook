53e46ec4be6507c94946a770759b6cd02d5d14be
PaulTA91 <parmst202@caledonian.ac.uk>
2022-03-07 16:38:47 +0000

    removed node_modules folder

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const router = require('./controllers/guestbookRoutes');
+const express = require("express");
+const router = require("./controllers/guestbookRoutes");
 const app = express();
 
-app.use('/', router);
+const path = require("path");
+const public = path.join(__dirname, "public");
+app.use(express.static(public));
 
-app.listen(3000, () =>{
-    console.log('Server started on port 3000. Ctrl^C to quit.');
-})
+app.use("/", router);
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000. Ctrl^C to quit.");
+});
