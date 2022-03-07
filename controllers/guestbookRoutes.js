7885cdb01ae1f4567bbf6ca8c226121c82f3b621
PaulTA91 <parmst202@caledonian.ac.uk>
2022-03-07 16:33:11 +0000

    added guestbook handler

--- controllers/guestbookRoutes.js
+++ controllers/guestbookRoutes.js
@@ -1,8 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get("/", function(req,res){
-    res.send('Hello! Welcome to the guestbook application');
-})
+router.get("/", function (req, res) {
+  res.send("Hello! Welcome to the guestbook application");
+});
+
+router.get("/guestbook", function (req, res) {
+  res.send("<h1>Guestbook Messages</h1>");
+});
 
 module.exports = router;
